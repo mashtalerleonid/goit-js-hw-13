ee848400d5671ee10327c0c2c9bf6521e2ecedb1
mashtalerleonid <mashtalerleonid@gmail.com>
2021-07-16 16:41:59 +0300

    in progres....

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ import imageCardTpl from './templates/image-card.hbs';
 import ImagesApiService from './js/image-service';
 import Notiflix from 'notiflix';
 
+const imagesApiService = new ImagesApiService();
+
 const refs = {
   searchForm: document.querySelector('.search-form'),
   imagesContainer: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
 };
 
 refs.searchForm.addEventListener('submit', onSearch);
-
-const imagesApiService = new ImagesApiService();
+refs.loadMoreBtn.addEventListener('click', fetchImages);
 
 function onSearch(e) {
   e.preventDefault();
-  console.log(imagesApiService);
+
+  refs.loadMoreBtn.classList.add('is-hidden');
 
   imagesApiService.query = e.currentTarget.elements.searchQuery.value;
 
@@ -24,23 +27,38 @@ function onSearch(e) {
   }
 
   imagesApiService.resetPage();
+
   clearArticlesContainer();
+
   fetchImages();
 }
 
-async function fetchImages(images) {
+async function fetchImages() {
   try {
-    Notiflix.Loading.arrows('Loading...');
+    showLoading();
 
     const fetchedImages = await imagesApiService.fetchImages();
-    // console.log(fetchedImages);
-    Notiflix.Loading.remove();
 
-    makeNotificationSuccess(`Hooray! We found ${fetchedImages.totalHits} images.`);
+    hideLoading();
+
+    const totalHits = fetchedImages.totalHits;
+    const currentPage = imagesApiService.options.params.page - 1;
+    const perPage = imagesApiService.options.params.per_page;
+    // console.log(fetchedImages);
+
+    if (currentPage === 1) {
+      makeNotificationSuccess(`Hooray! We found ${totalHits} images.`);
+    }
+
     appendMarkup(fetchedImages);
+
+    checkImagesCount(totalHits, currentPage, perPage);
+
+    smoothScroll();
   } catch (error) {
-    // console.log(error);
-    Notiflix.Loading.remove();
+    console.log(error);
+
+    hideLoading();
 
     makeNotificationError(
       'Sorry, there are no images matching your search query. Please try again.',
@@ -56,10 +74,44 @@ function makeNotificationSuccess(text) {
   Notiflix.Notify.success(text);
 }
 
+function makeNotificationInfo(text) {
+  Notiflix.Notify.info(text);
+}
+
 function makeNotificationError(text) {
   Notiflix.Notify.failure(text);
 }
 
+function showLoading() {
+  Notiflix.Loading.arrows('Loading...');
+}
+
+function hideLoading() {
+  Notiflix.Loading.remove();
+}
+
 function clearArticlesContainer() {
   refs.imagesContainer.innerHTML = '';
+}
+
+function smoothScroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  if (imagesApiService.options.params.page !== 2) {
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+  }
+}
+
+function checkImagesCount(total, current, per) {
+  if (current * per >= total) {
+    makeNotificationInfo("We're sorry, but you've reached the end of search results.");
+    refs.loadMoreBtn.classList.add('is-hidden');
+  } else {
+    refs.loadMoreBtn.classList.remove('is-hidden');
+  }
 }
